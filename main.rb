44bfcbc98b1934797a6bf421d8598091bc7cbac9
Steven Thiesfeld <scthiesfeld@hotmail.com>
2015-02-11 14:01:49 -0600

    added creation options for objects

--- main.rb
+++ main.rb
@@ -13,11 +13,8 @@ helpers do
   def categories
     @categories = Category.all("categories")
   end
-  
-end
+end#helpers_end
 
-
-    
 
 get "/" do
   erb :homepage
@@ -27,13 +24,7 @@ get "/locations" do
   locations
   erb :locations
 end
-
-get "/location_details" do
-  locations
-end
   
-  
-
 get "/products" do
   products
   erb :products
@@ -43,3 +34,32 @@ get "/categories" do
   categories
   erb :categories
 end
+
+get "/create_location" do
+  erb :create_location
+end
+
+get "/create_product" do
+  erb :create_product
+end
+
+get "/create_category" do
+  erb :create_category
+end
+
+get "/confirm_creation" do
+  if request.referrer == "http://127.0.0.1:4567/create_location"
+    @new_creation = Location.new(params) 
+    @new_creation.insert("locations")
+  elsif request.referrer == "http://127.0.0.1:4567/create_product"
+    @new_creation = Product.new(params)
+    @new_creation.insert("products")
+  elsif request.referrer == "/http://127.0.0.1:4567/create_category"
+    @new_creation = Category.new(params)
+    @new_creation.insert("categories")
+  end
+  erb :confirm_creation 
+end
+
+
+
