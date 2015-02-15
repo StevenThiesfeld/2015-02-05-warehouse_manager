809b221b1d74b4257c7f6fc3f7f5cb0e4a8addd4
Steven Thiesfeld <scthiesfeld@hotmail.com>
2015-02-14 21:45:08 -0600

    path change

--- tests/test_warehouse_manager.rb
+++ tests/test_warehouse_manager.rb
@@ -1,6 +1,6 @@
 require 'sqlite3'
 require 'pry'
-DATABASE = SQLite3::Database.new('database/test_warehouse_database.db')
+DATABASE = SQLite3::Database.new('../database/test_warehouse_database.db')
 require 'minitest/autorun'
 require_relative "../database/database_methods"
 require_relative "../helper_modules/model_helper"
